744c4cf5149f858e6c0b155f9292f2b83443e725
adri <adri.saputra.ibrahim@gmail.com>
2021-11-16 07:51:58 +0800

    Rekapitulasi

--- app/Http/Controllers/RekapitulasiController.php
+++ app/Http/Controllers/RekapitulasiController.php
@@ -2,10 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\RiwayatGolongan;   //nama model
-use App\Models\RiwayatPendidikan;   //nama model
-use App\Models\Jabatan;   //nama model
-use App\Models\Bidang;   //nama model
 use App\Models\Pegawai;   //nama model
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
@@ -25,88 +21,71 @@ class RekapitulasiController extends Controller
     public function golongan_pegawai()
     {
         $title = "Rekapitulasi Golongan Pegawai";
-        $gol1a = RiwayatGolongan::where('id_golongan',11)->count();
-        $gol1b = RiwayatGolongan::where('id_golongan',12)->count();
-        $gol1c = RiwayatGolongan::where('id_golongan',13)->count();
-        $gol1d = RiwayatGolongan::where('id_golongan',14)->count();
+        $gol1a = Pegawai::where('id_gol_ruang_akhir',11)->count();
+        $gol1b = Pegawai::where('id_gol_ruang_akhir',12)->count();
+        $gol1c = Pegawai::where('id_gol_ruang_akhir',13)->count();
+        $gol1d = Pegawai::where('id_gol_ruang_akhir',14)->count();
 
-        $gol2a = RiwayatGolongan::where('id_golongan',21)->count();
-        $gol2b = RiwayatGolongan::where('id_golongan',22)->count();
-        $gol2c = RiwayatGolongan::where('id_golongan',23)->count();
-        $gol2d = RiwayatGolongan::where('id_golongan',24)->count();
+        $gol2a = Pegawai::where('id_gol_ruang_akhir',21)->count();
+        $gol2b = Pegawai::where('id_gol_ruang_akhir',22)->count();
+        $gol2c = Pegawai::where('id_gol_ruang_akhir',23)->count();
+        $gol2d = Pegawai::where('id_gol_ruang_akhir',24)->count();
 
-        $gol3a = RiwayatGolongan::where('id_golongan',31)->count();
-        $gol3b = RiwayatGolongan::where('id_golongan',32)->count();
-        $gol3c = RiwayatGolongan::where('id_golongan',33)->count();
-        $gol3d = RiwayatGolongan::where('id_golongan',34)->count();
+        $gol3a = Pegawai::where('id_gol_ruang_akhir',31)->count();
+        $gol3b = Pegawai::where('id_gol_ruang_akhir',32)->count();
+        $gol3c = Pegawai::where('id_gol_ruang_akhir',33)->count();
+        $gol3d = Pegawai::where('id_gol_ruang_akhir',34)->count();
 
-        $gol4a = RiwayatGolongan::where('id_golongan',41)->count();
-        $gol4b = RiwayatGolongan::where('id_golongan',42)->count();
-        $gol4c = RiwayatGolongan::where('id_golongan',43)->count();
-        $gol4d = RiwayatGolongan::where('id_golongan',44)->count();
-        $gol4e = RiwayatGolongan::where('id_golongan',45)->count();
-
-        echo $gol1a;
-        // return view('admin.rekapitulasi.golongan_pegawai');
+        $gol4a = Pegawai::where('id_gol_ruang_akhir',41)->count();
+        $gol4b = Pegawai::where('id_gol_ruang_akhir',42)->count();
+        $gol4c = Pegawai::where('id_gol_ruang_akhir',43)->count();
+        $gol4d = Pegawai::where('id_gol_ruang_akhir',44)->count();
+        $gol4e = Pegawai::where('id_gol_ruang_akhir',45)->count();
 
     }
     
     public function pendidikan_pegawai()
     {
         $title = "Rekapitulasi Pendidikan Pegawai";
-        $sd = RiwayatPendidikan::select(DB::raw('max(id_tingkat_pendidikan) as id_tingkat_pendidikanx'))
-                ->having('id_tingkat_pendidikanx', 05)
-                ->orderBy('id_tingkat_pendidikan', 'desc')
-                ->groupBy('nip')->count();
-        $sltp = RiwayatPendidikan::select(DB::raw('max(id_tingkat_pendidikan) as id_tingkat_pendidikanx'))
-                ->having('id_tingkat_pendidikanx', 10)
-                ->orderBy('id_tingkat_pendidikan', 'desc')
-                ->groupBy('nip')->count();
-        $sltp_kejuruan = RiwayatPendidikan::select(DB::raw('max(id_tingkat_pendidikan) as id_tingkat_pendidikanx'))
-                ->having('id_tingkat_pendidikanx', 12)
-                ->orderBy('id_tingkat_pendidikan', 'desc')
-                ->groupBy('nip')->count();
-        $slta = RiwayatPendidikan::select(DB::raw('max(id_tingkat_pendidikan) as id_tingkat_pendidikanx'))
-                ->having('id_tingkat_pendidikanx', 15)
-                ->orderBy('id_tingkat_pendidikan', 'desc')
-                ->groupBy('nip')->count();
-        $slta_kejuruan = RiwayatPendidikan::select(DB::raw('max(id_tingkat_pendidikan) as id_tingkat_pendidikanx'))
-                ->having('id_tingkat_pendidikanx', 17)
-                ->orderBy('id_tingkat_pendidikan', 'desc')
-                ->groupBy('nip')->count();
-        $slta_keguruan = RiwayatPendidikan::select(DB::raw('max(id_tingkat_pendidikan) as id_tingkat_pendidikanx'))
-                ->having('id_tingkat_pendidikanx', 18)
-                ->orderBy('id_tingkat_pendidikan', 'desc')
-                ->groupBy('nip')->count();
-        $diploma1 = RiwayatPendidikan::select(DB::raw('max(id_tingkat_pendidikan) as id_tingkat_pendidikanx'))
-                ->having('id_tingkat_pendidikanx', 20)
-                ->orderBy('id_tingkat_pendidikan', 'desc')
-                ->groupBy('nip')->count();
-        $diploma2 = RiwayatPendidikan::select(DB::raw('max(id_tingkat_pendidikan) as id_tingkat_pendidikanx'))
-                ->having('id_tingkat_pendidikanx', 25)
-                ->orderBy('id_tingkat_pendidikan', 'desc')
-                ->groupBy('nip')->count();
-        $diploma3 = RiwayatPendidikan::select(DB::raw('max(id_tingkat_pendidikan) as id_tingkat_pendidikanx'))
-                ->having('id_tingkat_pendidikanx', 30)
-                ->orderBy('id_tingkat_pendidikan', 'desc')
-                ->groupBy('nip')->count();
-        $diploma4 = RiwayatPendidikan::select(DB::raw('max(id_tingkat_pendidikan) as id_tingkat_pendidikanx'))
-                ->having('id_tingkat_pendidikanx', 35)
-                ->orderBy('id_tingkat_pendidikan', 'desc')
-                ->groupBy('nip')->count();
-        $s1 = RiwayatPendidikan::select(DB::raw('max(id_tingkat_pendidikan) as id_tingkat_pendidikanx'))
-                ->having('id_tingkat_pendidikanx', 40)
-                ->orderBy('id_tingkat_pendidikan', 'desc')
-                ->groupBy('nip')->count();
-        $s2 = RiwayatPendidikan::select(DB::raw('max(id_tingkat_pendidikan) as id_tingkat_pendidikanx'))
-                ->having('id_tingkat_pendidikanx', 45)
-                ->orderBy('id_tingkat_pendidikan', 'desc')
-                ->groupBy('nip')->count();
-        $s3 = RiwayatPendidikan::select(DB::raw('max(id_tingkat_pendidikan) as id_tingkat_pendidikanx'))
-                ->having('id_tingkat_pendidikanx', 50)
-                ->orderBy('id_tingkat_pendidikan', 'desc')
-                ->groupBy('nip')->count();
+        $sd = Pegawai::where('id_tingkat_pendidikan_terakhir', 05)->count();
+        $sltp = Pegawai::where('id_tingkat_pendidikan_terakhir', 10)->count();
+        $sltp_kejuruan = Pegawai::where('id_tingkat_pendidikan_terakhir', 12)->count();
+        $slta = Pegawai::where('id_tingkat_pendidikan_terakhir', 15)->count();
+        $slta_kejuruan = Pegawai::where('id_tingkat_pendidikan_terakhir', 17)->count();
+        $slta_keguruan = Pegawai::where('id_tingkat_pendidikan_terakhir', 18)->count();
+        $diploma1 = Pegawai::where('id_tingkat_pendidikan_terakhir', 20)->count();
+        $diploma2 = Pegawai::where('id_tingkat_pendidikan_terakhir', 25)->count();
+        $diploma3 = Pegawai::where('id_tingkat_pendidikan_terakhir', 30)->count();
+        $diploma4 = Pegawai::where('id_tingkat_pendidikan_terakhir', 35)->count();
+        $s1 = Pegawai::where('id_tingkat_pendidikan_terakhir', 40)->count();
+        $s2 = Pegawai::where('id_tingkat_pendidikan_terakhir', 45)->count();
+        $s3 = Pegawai::where('id_tingkat_pendidikan_terakhir', 50)->count();
 
+    }
+    
+    public function esselon_pegawai()
+    {
+        $title = "Rekapitulasi Esselon Pegawai";
+        $esselon10 = Pegawai::where('id_esselon', 10)->count();
+        $esselon11 = Pegawai::where('id_esselon', 11)->count();
+        $esselon12 = Pegawai::where('id_esselon', 12)->count();
+        $esselon21 = Pegawai::where('id_esselon', 21)->count();
+        $esselon22 = Pegawai::where('id_esselon', 22)->count();
+        $esselon31 = Pegawai::where('id_esselon', 31)->count();
+        $esselon32 = Pegawai::where('id_esselon', 32)->count();
+        $esselon41 = Pegawai::where('id_esselon', 41)->count();
+        $esselon42 = Pegawai::where('id_esselon', 42)->count();
+        $esselon51 = Pegawai::where('id_esselon', 51)->count();
+        $esselon52 = Pegawai::where('id_esselon', 52)->count();
+        $esselon99 = Pegawai::where('id_esselon', 99)->count();
+
+    }
+    
+    public function Jumlah_pegawai()
+    {
+        $title = "Rekapitulasi Jumlah Pegawai";
+        $jumlah_pegawai = Pegawai::count();
+        return view('admin.rekapitulasi.gender_pegawai',compact('title','jumlah_pegawai'));
     }
     
     public function gender_pegawai()
